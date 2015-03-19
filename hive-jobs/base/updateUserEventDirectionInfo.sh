32bf205d6027745515599e6016f9f4507941769d
Ankur Aggarwal <ankura@bsb.in>
2015-03-19 19:13:45 +0530

    Changes in the hive queries

--- hive-jobs/base/updateUserEventDirectionInfo.sh
+++ hive-jobs/base/updateUserEventDirectionInfo.sh
@@ -19,26 +19,29 @@ create temporary function sessionnum as 'com.bsb.portal.sojourn.hive.udf.Session
 create temporary function sourceevent as 'com.bsb.portal.sojourn.hive.udf.SourceEvent' ;
 
 INSERT OVERWRITE TABLE sj_user_event_direction_info  PARTITION(day='$DAYSTR')
-SELECT user_id , sessionnum(timestr,user_id) as sessionNum ,sourceevent(timestr,user_id,event_id) as source_event_id,event_id as destination_event_id
+SELECT * 
 FROM
 (
-    SELECT T1.user_id as user_id, T1.timestr as timestr,T2.id as event_id
+    SELECT T3.user_id as user_id , sessionnum(T3.timestr,T3.user_id) as session_num ,sourceevent(T3.timestr,T3.user_id,T3.event_id) as source_event_id,T3.event_id as destination_event_id
+    FROM
     (
-        SELECT DISTINCT user_id,screen_id,action_id,timestr 
-        FROM twang.ext_analytics_log 
-        WHERE (day = '$DAYSTR') AND (user_id IS NOT NULL) AND (timestr IS NOT NULL)
-    )T1
-    INNER JOIN 
-    (
-        SELECT * FROM sj_event_info
-    )T2
-    ON (T1.action_id = T2.action_id) AND (T1.screen_id = T2.screen_id)
-    ORDER BY user_id,timestr
-)T3
-WHERE source_event_id > 0;
+        SELECT T1.user_id as user_id, T1.timestr as timestr,T2.id as event_id
+        FROM
+        (
+            SELECT DISTINCT user_id,screen_id,action_id,timestr 
+            FROM twang.ext_analytics_log 
+            WHERE (day = '$DAYSTR') AND (user_id IS NOT NULL) AND  (user_id != '-') AND (timestr IS NOT NULL)
+        )T1
+        INNER JOIN 
+        (
+            SELECT * FROM sj_event_info
+        )T2
+        ON (T1.action_id = T2.action_id) AND (T1.screen_id = T2.screen_id)
+        ORDER BY user_id,timestr
+    )T3
+  )T4
+WHERE T4.source_event_id > 0;
 
 "
 
 
-
-#!/bin/bash
--- hive-jobs/base/updateUserJourneyInfo.sh
+++ hive-jobs/base/updateUserJourneyInfo.sh
@@ -14,14 +14,15 @@ echo "Running for DAYSTR=${DAYSTR}";
 hive DAYSTR=${DAYSTR} -d HOME=${HOME} -v -e "
 use sojourn;
 INSERT OVERWRITE TABLE sj_user_journey_info  PARTITION(day='$DAYSTR')
-SELECT user_id , event_id , collect_set(timestr) as timestr_array,collect_set(item_id) as song_id_array
+SELECT T3.user_id as user_id, T3.event_id as event_id , collect_set(T3.timestr) as timestr_array,collect_set(T3.item_id) as song_id_array
+FROM
 (
     SELECT T1.user_id as user_id,T2.id as event_id,T1.item_id as item_id,T1.timestr as timestr
     FROM 
     (
         SELECT DISTINCT user_id,item_id,screen_id,action_id,timestr 
         FROM twang.ext_analytics_log 
-        WHERE day = '$DAYSTR' AND user_id IS NOT NULL
+        WHERE (day = '$DAYSTR' ) AND (user_id IS NOT NULL) AND (user_id != '-') AND (timestr IS NOT NULL)
     ) T1
     INNER JOIN 
     (
@@ -30,7 +31,7 @@ SELECT user_id , event_id , collect_set(timestr) as timestr_array,collect_set(it
     ON (T1.action_id = T2.action_id) AND (T1.screen_id = T2.screen_id)
     ORDER BY user_id,event_id,timestr
 )T3
-GROUP BY user_id, event_id
+GROUP BY T3.user_id, T3.event_id
 
 "
 
